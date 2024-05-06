c157e251a69fcd823d5ed742c3782ab4f4ddce59
fakemonstro <62348132+gustavomiguel33@users.noreply.github.com>
2024-05-06 20:13:21 -0300

    Continuando o projeto
    Adicionado mais alguns textos a página sobre mim e adicionado o link das páginas.

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var projects = document.querySelector('.projects')
+
+projects.addEventListener('click', function() {
+    window.location.href = 'https://github.com/gustavomiguel33?tab=repositories'
+})
