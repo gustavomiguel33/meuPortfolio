cc9b1ae761fba48d14b829cfc033d9f7cb10577f
fakemonstro <62348132+gustavomiguel33@users.noreply.github.com>
2024-05-07 10:30:33 -0300

    .
    .

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var projects = document.querySelector('.projects')
+const projects = document.querySelector('.projects')
 
 projects.addEventListener('click', function() {
     window.location.href = 'https://github.com/gustavomiguel33?tab=repositories'
